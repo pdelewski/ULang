4e6e3b10d639a7a41f772a72dd197a28b029ad1a
Przemek Delewski <przemyslaw_delewski@o2.pl>
2025-01-27 20:34:57 +0100

    Headers generation formatting

--- ulc/cpp_backend.go
+++ ulc/cpp_backend.go
@@ -940,7 +940,13 @@ func (v *CppBackend) ProLog() {
 		fmt.Println("Error opening file:", err)
 		return
 	}
-	_, err = v.file.WriteString("#include <vector>\n#include <string>\n#include <tuple>\n#include <any>\n#include <cstdint>\n#include <functional>\n#include \"../builtins/builtins.h\"\n\n")
+	_, err = v.file.WriteString("#include <vector>\n" +
+		"#include <string>\n" +
+		"#include <tuple>\n" +
+		"#include <any>\n" +
+		"#include <cstdint>\n" +
+		"#include <functional>\n" +
+		"#include \"../builtins/builtins.h\"\n\n")
 	if err != nil {
 		fmt.Println("Error writing to file:", err)
 		return
