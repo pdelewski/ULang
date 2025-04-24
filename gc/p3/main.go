0ee9f3497581fb503ca4a50cc86d9128de8b4d15
Przemek Delewski <przemyslaw_delewski@o2.pl>
2025-04-24 21:14:56 +0200

    Adding memory management ideas

---
+++ gc/p3/main.go
@@ -0,0 +1,80 @@
+package main
+
+import "fmt"
+
+// Allocator manages arena-backed int storage via index
+type Allocator struct {
+    arena    []int
+    freeList []int
+    nextFree int
+}
+
+// NewAllocator creates an allocator with a fixed arena size
+func NewAllocator(capacity int) *Allocator {
+    return &Allocator{
+	arena:    make([]int, capacity),
+	freeList: make([]int, 0, capacity),
+	nextFree: 0,
+    }
+}
+
+// Alloc returns a free index from the arena
+func (a *Allocator) Alloc() int {
+    var index int
+    if len(a.freeList) > 0 {
+	// Reuse from freelist
+	index = a.freeList[len(a.freeList)-1]
+	a.freeList = a.freeList[:len(a.freeList)-1]
+    } else if a.nextFree < len(a.arena) {
+	// Allocate new slot
+	index = a.nextFree
+	a.nextFree++
+    } else {
+	fmt.Println("Allocator out of memory")
+	return -1
+    }
+    return index
+}
+
+// Free returns an index to the free list for reuse
+func (a *Allocator) Free(index int) {
+    if index >= 0 && index < len(a.arena) {
+	a.freeList = append(a.freeList, index)
+    }
+}
+
+// Arena gives access to the underlying data
+func (a *Allocator) Arena() []int {
+    return a.arena
+}
+
+// Example function using the allocator
+func foo(alloc *Allocator) {
+    index := alloc.Alloc()
+    if index == -1 {
+	return
+    }
+
+    arena := alloc.Arena()
+    arena[index] = 42
+    fmt.Printf("Allocated value at index %d: %d\n", index, arena[index])
+
+    // Simulate we're done with this value
+    alloc.Free(index)
+    fmt.Printf("Freed index %d\n", index)
+}
+
+func bar(alloc *Allocator) {
+    x := new(int)
+    *x = 42
+    fmt.Println("Value inside foo:", *x)
+}
+
+// Main test
+func main() {
+    alloc := NewAllocator(5)
+
+    for i := 0; i < 7; i++ {
+	foo(alloc)
+    }
+}
