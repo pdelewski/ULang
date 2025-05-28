128cab7b14655d133c35f578e055383373605f1a
Przemek Delewski <przemyslaw_delewski@o2.pl>
2025-05-28 20:40:25 +0200

    Adding and using astyle

--- ulc/main.go
+++ ulc/main.go
@@ -1,10 +1,30 @@
+//go:generate make -C astyle astyle
+
 package main
 
 import (
 	"flag"
 	"fmt"
 	"golang.org/x/tools/go/packages"
+	"log"
+	"os"
+	"os/exec"
+	"strings"
 )
+
+func RunCommand(command string) error {
+	parts := strings.Fields(command)
+	if len(parts) == 0 {
+		return nil
+	}
+
+	cmd := exec.Command(parts[0], parts[1:]...)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	log.Printf("Running: %s\n", command)
+	return cmd.Run()
+}
 
 func main() {
 	var sourceDir string
@@ -48,7 +68,13 @@ func main() {
 	passManager.passes = append(passManager.passes, csBackend)
 
 	passManager.RunPasses()
+	err = RunCommand("./astyle/astyle --style=webkit output.cpp")
+	if err != nil {
+		log.Fatalf("Command failed: %v", err)
+	}
+	err = RunCommand("./astyle/astyle --style=webkit Program.cs")
+	if err != nil {
+		log.Fatalf("Command failed: %v", err)
+	}
 
-	format("output.cpp", "formatted.xx")
-	format("Program.cs", "formatted.cs")
 }
